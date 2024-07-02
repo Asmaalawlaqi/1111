b10e3c02479cace614e97616c715e807bfd61184
Asmaalawlaqi <143976152+Asmaalawlaqi@users.noreply.github.com>
2024-07-02 23:17:07 +0400

    Create w.c

---
+++ w.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+
+int main(int ac , char **av)
+{
+int i = 0;
+int j = 0;
+if(ac == 3)
+{ 
+	while(av[2][i])
+		{
+if(av[1][j] == av[2][i])
+	j++;
+if(av[1][j] == '\0')
+{
+	j = 0;
+
+while(av[1][j])
+	{
+		write(1, &av[1][i] , 1);
+		j++;
+	}
+	break;
+}
+			i++;
+	
+
+}
+write(1, "\n" , 1);
+return(0);
+}
